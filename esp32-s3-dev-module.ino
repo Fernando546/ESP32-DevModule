95728ecd83b1698c863d50d766c4a3d8e74708ef
Dawid Feruś <ferero546@gmail.com>
2024-09-22 02:04:23 +0200

    Added global time synchronization and deep sleep mode

--- esp32-s3-dev-module.ino
+++ esp32-s3-dev-module.ino
@@ -1,6 +1,9 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <DHT.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <esp_sleep.h>
 
 #define DHTPIN 16
 #define DHTTYPE DHT22
@@ -10,9 +13,8 @@ const char* password = "PASSWORD";
 const char* serverName = "https://esp32-frontend.vercel.app/api/data"; 
 
 DHT dht(DHTPIN, DHTTYPE);
-
-unsigned long previousMillis = 0;  
-const long interval = 1800000;     
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000);
 
 void setup() {
   Serial.begin(115200);
@@ -24,43 +26,81 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("Connected to WiFi");
+
+  timeClient.begin();
+  timeClient.update(); 
+
+  Serial.print("Current time: ");
+  Serial.println(timeClient.getFormattedTime());
+
+  int currentMinute = timeClient.getMinutes();
+  int currentSecond = timeClient.getSeconds();
+
+  if (currentMinute % 30 == 0 && currentSecond < 30) {
+    Serial.println("Attempting to take a measurement...");
+    takeMeasurement();
+  } else {
+    Serial.println("Not the right time for measurement");
+  }
+
+  unsigned long sleepTime = calculateSleepTime();
+
+  esp_sleep_enable_timer_wakeup(sleepTime * 1000000); 
+  Serial.println("Going to sleep...");
+  esp_deep_sleep_start();
+}
+
+void takeMeasurement() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  HTTPClient http;
+  http.begin(serverName);
+  http.addHeader("Content-Type", "application/json");
+
+  String payload = "{\"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + "}";
+  int httpResponseCode = http.POST(payload);
+
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    Serial.println("Response Code: " + String(httpResponseCode));
+    Serial.println("Response: " + response);
+  } else {
+    Serial.println("Error on sending POST request");
+    Serial.println("Response Code: " + String(httpResponseCode));
+  }
+
+  http.end();
+}
+
+unsigned long calculateSleepTime() {
+  timeClient.update();
+  int currentMinute = timeClient.getMinutes();
+  int currentSecond = timeClient.getSeconds();
+
+  int minutesToNextHalfHour = 30 - (currentMinute % 30); 
+  int secondsToNextMinute = 60 - currentSecond; 
+
+  unsigned long totalSleepTime = (minutesToNextHalfHour * 60) + secondsToNextMinute - 30;
+
+  Serial.printf("Going to sleep for: %d min, %d s\n", totalSleepTime / 60, totalSleepTime % 60);
+  
+  timeClient.update();
+  String predictedWakeTime = timeClient.getFormattedTime();
+  int predictedHour = (predictedWakeTime.substring(0, 2).toInt() + (currentMinute + minutesToNextHalfHour) / 60) % 24;
+  int predictedMinute = (currentMinute + minutesToNextHalfHour) % 60;
+  predictedWakeTime = String(predictedHour) + ":" + String(predictedMinute < 10 ? "0" : "") + String(predictedMinute) + ":30";
+
+  Serial.println("Predicted wake-up time: " + predictedWakeTime);
+  
+  return totalSleepTime;
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  
-  if (WiFi.status() == WL_CONNECTED) {
-    if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-
-      float temperature = dht.readTemperature();
-      float humidity = dht.readHumidity();
-
-      if (isnan(temperature) || isnan(humidity)) {
-        Serial.println("Failed to read from DHT sensor!");
-        return;
-      }
-
-      HTTPClient http;
-      http.begin(serverName);
-      http.addHeader("Content-Type", "application/json");
-
-      String payload = "{\"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + "}";
-      
-      int httpResponseCode = http.POST(payload);
-
-      if (httpResponseCode > 0) {
-        String response = http.getString();
-        Serial.println("Response Code: " + String(httpResponseCode));
-        Serial.println("Response: " + response);
-      } else {
-        Serial.println("Error on sending POST request");
-        Serial.println("Response Code: " + String(httpResponseCode));
-      }
-
-      http.end();
-    }
-  } else {
-    Serial.println("Error in WiFi connection");
-  }
+  // Nic się tutaj nie dzieje
 }
